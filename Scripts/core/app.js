a92db72ae5b4bf00209f746810e231c572604920
JCannon1 <jessecannon1@hotmail.com>
2017-04-19 12:11:21 -0400

    edited canvas height and added in labels and the button

--- Scripts/core/app.js
+++ Scripts/core/app.js
@@ -1,6 +1,21 @@
+/* File name: app.js */
+/* Author's name: Jesse Cannon */
+/* Student ID: 200348532 */
+/* Web site name: http://comp1073-w2017-finalexam-200348532.azurewebsites.net//index.html */
+/* File description: JavaScript page for final exam */
+
 // IIFE - Immediately Invoked Function Expression
 (function () {
+    var canvas;
+    var stage;
+    var firstDieLabel;
+    var secondDieLabel;
+    var rollButton;
+
+
     function Start() {
+        canvas = document.getElementById("canvas");
+
         Main(); // call the main function
     }
     function Update(event) {
