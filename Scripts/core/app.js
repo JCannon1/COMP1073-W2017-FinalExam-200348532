8edccac34778f780a291e18f18948e27d7edbe61
JCannon1 <jessecannon1@hotmail.com>
2017-04-19 12:30:13 -0400

    made roll button and labels appear on canvas

--- Scripts/core/app.js
+++ Scripts/core/app.js
@@ -1,9 +1,3 @@
-/* File name: app.js */
-/* Author's name: Jesse Cannon */
-/* Student ID: 200348532 */
-/* Web site name: http://comp1073-w2017-finalexam-200348532.azurewebsites.net//index.html */
-/* File description: JavaScript page for final exam */
-
 // IIFE - Immediately Invoked Function Expression
 (function () {
     var canvas;
@@ -11,16 +5,37 @@
     var firstDieLabel;
     var secondDieLabel;
     var rollButton;
-
-
     function Start() {
         canvas = document.getElementById("canvas");
-
+        // creates a new stage container - parent container for our app
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 30; // set framerate to 30 FPS
+        createjs.Ticker.on("tick", Update); // call the Update method every frame
         Main(); // call the main function
     }
     function Update(event) {
+        firstDieLabel.x = firstDieLabel.x + 5; // move the text to the right of the canvas every frame
+        secondDieLabel.y = secondDieLabel.y + 7; // move the text to the bottom of the canvas every frame
+        stage.update(); // redraw the stage
     }
     function Main() {
+        firstDieLabel = new objects.Label("Click on Projects! For Star Wars Movie Poster", "20px", "Consolas", "#000000", 125, 125, true);
+        stage.addChild(firstDieLabel);
+        // add a goodbyeLabel to the stage
+        secondDieLabel = new objects.Label("Or Click Projects Button!", "24px", "Arial", "#FF0000", 125, 125, true);
+        stage.addChild(secondDieLabel);
+        // add a rollButton to the stage
+        rollButton = new createjs.Bitmap("../../Assets/images/rollButton.png");
+        rollButton.regX = rollButton.getBounds().width * 0.5;
+        rollButton.regY = rollButton.getBounds().height * 0.5;
+        rollButton.x = 125;
+        rollButton.y = 200;
+        stage.addChild(rollButton);
+        
+        rollButton.on("click", function () {
+            firstDieLabel.text = "Click on Projects! For Star Wars Movie Poster";
+            secondDieLabel.text = "Or Click Projects Button!";
+        });
     }
     // window binding events
     window.onload = Start;
--- Scripts/core/app.ts
+++ Scripts/core/app.ts
@@ -1,7 +1,20 @@
 // IIFE - Immediately Invoked Function Expression
 (function () {
 
+  let canvas:HTMLElement;
+  let stage:createjs.Stage;
+  let firstDieLabel:objects.Label;
+  let secondDieLabel:objects.Label;
+
+  let rollButton:createjs.Bitmap;
+
   function Start(): void {
+
+    canvas = document.getElementById("canvas");
+    // creates a new stage container - parent container for our app
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.framerate = 30; // set framerate to 30 FPS
+    createjs.Ticker.on("tick", Update); // call the Update method every frame
 
 
     Main(); // call the main function
@@ -10,13 +23,36 @@
 
   function Update(event: createjs.Event): void {
 
+     firstDieLabel.x = firstDieLabel.x + 5; // move the text to the right of the canvas every frame
+
+     secondDieLabel.y = secondDieLabel.y + 7; // move the text to the bottom of the canvas every frame
+
+     stage.update(); // redraw the stage
 
   }
 
 
   function Main(): void {
 
+    firstDieLabel = new objects.Label("Click on Projects! For Star Wars Movie Poster", "20px", "Consolas", "#000000", 125, 125, true);
+    stage.addChild(firstDieLabel);
 
+    // add a goodbyeLabel to the stage
+    secondDieLabel = new objects.Label("Or Click Projects Button!", "24px", "Arial", "#FF0000", 125, 125, true);
+    stage.addChild(secondDieLabel);
+
+    // add a rollButton to the stage
+    rollButton = new createjs.Bitmap("../../Assets/images/rollButton.png");
+    rollButton.regX = rollButton.getBounds().width * 0.5;
+    rollButton.regY = rollButton.getBounds().height * 0.5;
+    rollButton.x = 125;
+    rollButton.y = 200;
+    stage.addChild(rollButton);
+
+    rollButton.on("click", function() {
+    firstDieLabel.text = "Click on Projects! For Star Wars Movie Poster";
+    secondDieLabel.text = "Or Click Projects Button!";
+    });
   }
 
   // window binding events
